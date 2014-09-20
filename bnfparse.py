3384fef877f83b89f11a3adaebdbd5c7faa69523
Aaron Levine <aclevine@brandeis.edu>
2014-09-19 22:31:13 -0400

    fixed parse and generate

--- bnfparse.py
+++ bnfparse.py
@@ -18,7 +18,7 @@ class Stack(list):
 
 class BNFParser(object):
     """docstring for BNFParser"""
-    def __init__(self, string, repmax=0):
+    def __init__(self, string, repmax=2):
         super(BNFParser, self).__init__()
         self.string = string
         self.normalize()
@@ -27,81 +27,79 @@ class BNFParser(object):
             [split_on('=', rule) for rule in split_on(';', self.string)]
         )
         self.parse()
-    
+        #=======================================================================
+        # for key in self.rules.keys():
+        #     pprint( self.rules[key], 0)
+        #     print '=' * 10
+        #=======================================================================
+
+
+    def __str__(self):
+        return ' '.join(map(string.strip, self.generate(self.rules['<START>'])))
+
     def normalize(self):
         text = self.string
         text = re.sub('//.+', '', text) # strip comments        
-        text = re.sub('\n', ' ', text) # normalize whitespace
-        text = re.sub('\s+', ' ', text) # normalize whitespace
-        text = re.sub(r'\s*([\|\)\(\[\]\+\*]+)\s*', r'\1', text) # normalize whitespace
-        text = re.sub(r'\[', r'[(', text) # normalize whitespace
-        text = re.sub(r'\]', r')]', text) # normalize whitespace
+        # normalize whitespace
+        text = re.sub('\n', ' ', text) 
+        text = re.sub('\s+', ' ', text) 
+        text = re.sub(r'\s*([\|\+\*]+)\s*', r'\1', text)
+        #text = re.sub(r'\[', r'[(', text) 
+        #text = re.sub(r'\]', r')]', text)
         self.string = text
     
-    def __str__(self):
-        return ' '.join(map(string.strip, self.generate(self.rules['<START>'], [])))
-    
-    def generate(self, tree, output=[]):
+    def generate(self, tree):
         """Traverse the tree to generate a sentence licensed by the FSG."""
+        output = []
+        if tree.attrib.has_key('token'): # Terminal
+            output.append(tree.attrib['token']) 
+        if tree.attrib.has_key('rule'): # Terminal
+            output.extend(self.generate(self.rules[tree.attrib['rule']]))
+        if tree.attrib.has_key('one-of'):
+            child = random.choice(tree.children)
+            output.extend(self.generate(child))
         if tree.attrib.has_key('all-of'): # Conjunction
             for child in tree.children:
-                self.generate(child, output)
-        if tree.attrib.has_key('one-of'): # Disjunction
-            chosen = random.choice(tree.children)
-            self.generate(chosen, output)
+                output.extend(self.generate(child))
         if tree.attrib.has_key('repeat'):   # Repetition
             n = random.choice(tree.attrib['repeat'])
             for i in range(n):
                 for child in tree.children:
-                    self.generate(child, output)
-        if tree.attrib.has_key('token'):    # Terminal
-            output.append(tree.attrib['token'])
-            for child in tree.children:
-                self.generate(child, output)
-        if tree.attrib.has_key('rule'):
-            for child in self.rules[tree.attrib['rule']].children:
-                self.generate(child, output)
+                    output.extend(self.generate(child))
         return output
 
-    def parse(self, repmax=3):
+    def parse(self):
         """Convert bnf to an n-ary tree via a recursive-descent parse."""
         # Instantiate a stack to keep track of each nested level
         stack = Stack()
         for lhs, rhs in self.rules.iteritems():
             root = Tree({'all-of' : 'all-of'})
             # Instantiate a tree to store the rule expansion
+            stack.push(root)
             current = root
             # Iterate over all tokens in the right-hand side
             for token in tokenize(rhs):
                 if token =='(':   # We need to go deeper
                     temp = Tree({'all-of' : 'all-of'})
                     current.children.append(temp)
-                    stack.push(current)
+                    temp.parent = current
+                    stack.push(temp)
                     current = temp
                 elif token ==')': # Kick back up a level
-                    current = stack.pop()
+                    stack.pop()
+                    current = stack[-1]
                 elif token =='[': # Repeat once or not at all
                     temp = Tree({'repeat': [0, 1]})
                     current.children.append(temp)
-                    stack.push(current)
+                    temp.parent = current
+                    stack.push(temp)
                     current = temp
                 elif token ==']': # Kick back up a level
-                    current = stack.pop()
+                    stack.pop()
+                    current = stack[-1]
                 elif token == '|': # Disjunction
-                    temp = Tree({'one-of' : 'one-of'})
-                    temp.parent = current.parent
-                    if current.parent != None:
-                        current.parent.pop()
-                        current.parent.append(temp)
-                    temp.children = [current]
-                    #===========================================================
-                    # temp.parent = current
-                    # child = current.children.pop()
-                    # current.children.append(temp)
-                    # child.parent = temp
-                    # temp.children.append(child)
-                    #===========================================================
-                    current = temp
+                    stack[-1].attrib = {'one-of' : 'one-of'}
+                    current = stack[-1]
                 elif token == ' ': # Disjunction
                     temp = Tree({'all-of' : 'all-of'})
                     temp.parent = current
@@ -111,12 +109,12 @@ class BNFParser(object):
                     temp.children.append(child)
                     current = temp
                 elif token == '*': # Repeat zero or more times
-                    temp = Tree({'repeat': range(0, repmax+1)})
+                    temp = Tree({'repeat': range(0, self.repmax+1)})
                     temp.children = [current.children.pop()]
                     current.children.append(temp)
                     temp.parent = current            
                 elif token == '+': # Repeat one or more times
-                    temp = Tree({'repeat': range(1, repmax+1)})
+                    temp = Tree({'repeat': range(1, self.repmax+1)})
                     temp.children = [current.children.pop()]
                     current.children.append(temp)
                     temp.parent = current
@@ -136,11 +134,13 @@ class Tree(object):
         self.parent = None
         self.children = []
 
-    def __str__(self):
-        if self.children == []:
-            return str([y for y in self.attrib.iteritems()][0][1])
-        else:
-            return '\n'.join([str([y for y in self.attrib.iteritems()][0][1])]+[str(child) for child in self.children])
+    #===========================================================================
+    # def __str__(self):
+    #     if self.children == []:
+    #         return str([y for y in self.attrib.iteritems()][0][1])
+    #     else:
+    #         return '\n'.join([str([y for y in self.attrib.iteritems()][0][1])]+[str(child) for child in self.children])
+    #===========================================================================
                     
 def tokenize(rule):
     """Tokenizes raw Backus-Naur Form (BNF) content."""
@@ -166,17 +166,21 @@ def split_on(delimiter, s):
     return map(strip, filter(None, s.split(delimiter)))
        
        
+def pprint(tree, i=0):
+    print "\t"*i + str(tree.attrib)
+    for child in tree.children:
+        pprint(child, i+1)
+
+       
 if __name__ == "__main__":
 
     with open('../grammars/grammar.wbnf', 'r') as fo:
         text = fo.read()
 
     grammar = BNFParser(text)
-    print grammar
-    print grammar
-    print grammar
-    print grammar
-    print grammar
+    for i in range(10):
+        print grammar
+        #print grammar.generate(grammar.rules['<START>'])
     
 #if __name__ == "__main__":
 #    from sys import argv
